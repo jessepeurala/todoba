6ef13026dab2b21ea1c8500f1daa606bb4ab7a91
Jesse <jebe@daug.net>
2008-12-09 20:49:36 +0200

    New routes and rename of index.html

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :projects
 
   map.resources :tickets
+  
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.login  '/login', :controller => 'sessions', :action => 'new'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -41,13 +46,14 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "tickets"
 
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
+
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
